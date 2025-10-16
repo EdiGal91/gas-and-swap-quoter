dd5778e8500913fafca98d240bcc1788378436cd
EdiGal91 <47693118+EdiGal91@users.noreply.github.com>
2025-10-16 18:39:33 +0400

    init config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.getOrThrow<number>('PORT');
+  const server = await app.listen(port);
+
+  return server;
 }
-bootstrap();
+
+bootstrap()
+  .then((server) => {
+    const address = server.address();
+    console.log(`Server is running on port ${address?.port}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
